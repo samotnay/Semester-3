6d957fadb9fd99fe802d1b1c7697114c6c3e80a3
samotnay <163724036+samotnay@users.noreply.github.com>
2025-01-08 12:23:58 +0700

    JAVA JDBC

---
+++ samotnay/Insert.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samotnay;
+
+import java.awt.HeadlessException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author MABESH PROJECT
+ */
+public class Insert {
+    public void insert(String nim, String nama, String prodi, String alamat, String email, String notelp){
+        
+         try {
+            String sql = "INSERT INTO Mahasiswa VALUES ('" + nim + "','" + nama + "','" + prodi + "','" + alamat + "','" + email + "','" + notelp + "')";
+            java.sql.Connection conn = (Connection) konfig.configDB();
+            java.sql.PreparedStatement pstm = conn.prepareStatement(sql);
+            pstm.execute();
+            JOptionPane.showMessageDialog(null, "Proses Simpan Data Berhasil..");
+          
+        } catch (HeadlessException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        
+    }
+}
