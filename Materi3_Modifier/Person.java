486f43ee483169b560e7b02e22651338b21f002c
samotnay <163724036+samotnay@users.noreply.github.com>
2025-01-21 09:53:52 +0700

    Materi 3 Modifier

---
+++ Materi3_Modifier/Person.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Materi3_Modifier;
+
+/**
+ *
+ * @author HP
+ */
+public class Person {
+    String name;
+    
+    public void setName(String name){
+        this.name= name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
